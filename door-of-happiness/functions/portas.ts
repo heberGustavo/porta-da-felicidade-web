f59745b12aeb53c8e711f3d77be276f0712686e6
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-29 17:48:03 -0300

    Create function to create and update elements

---
+++ door-of-happiness/functions/portas.ts
@@ -0,0 +1,18 @@
+import PortaModel from "../model/portaModel";
+
+export function criarPortas(qtde: number, portaSelecionada: number): PortaModel[] {
+    return Array.from({ length: qtde }, (_, i) => {
+        const numero = i + 1;
+        const temPresente = numero === portaSelecionada;
+        return new PortaModel(numero, temPresente);
+    });
+}
+
+export function alterarPorta(portas: PortaModel[], portaModificada: PortaModel): PortaModel[] {
+    return portas.map(portaAtual => {
+        if (portaAtual.numero === portaModificada.numero)
+            return portaModificada;
+        else 
+            return portaModificada.aberta ? portaAtual : portaAtual.desselecionar();
+    })
+}
--- door-of-happiness/src/pages/index.tsx
+++ door-of-happiness/src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import Porta from "../../components/Porta";
 import PortaModel from "../../model/portaModel";
+import { alterarPorta, criarPortas } from "../../functions/portas";
 
 export default function Home() {
+  const [portas, setPortas] = useState(criarPortas(3, 2));
 
-  const [ porta, setPorta ] = useState(new PortaModel(1));
-
-  return (
-    <div style={{display: "flex"}}>
-      <Porta value={porta} onChange={novaPorta => setPorta(novaPorta)} />
-    </div>
-  );
+  const renderizarPortas = () => {
+    return portas.map((porta) => {
+      return (
+        <Porta
+          key={porta.numero}
+          value={porta}
+          onChange={(novaPorta) => {
+            setPortas(alterarPorta(portas, novaPorta));
+          }}
+        />
+      );
+    });
+  };
+  return <div style={{ display: "flex" }}>{renderizarPortas()}</div>;
 }
