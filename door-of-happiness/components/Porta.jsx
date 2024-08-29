3213fcd228638f778110790e10b15b34b0842282
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-29 11:38:43 -0300

    Create area and border in Porta

--- door-of-happiness/components/Porta.jsx
+++ door-of-happiness/components/Porta.jsx
@@ -1,9 +1,14 @@
 import styles from "../src/styles/Porta.module.css";
 
-export default function Porta(props){
-    return(
-        <div className={styles.porta}>
-            <div className={styles.numero}>3</div>
+export default function Porta(props) {
+    return (
+        <div className={styles.area}>
+            <div className={styles.estrutura}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                </div>
+            </div>
+            <div className={styles.chao}></div>
         </div>
     );
 }
