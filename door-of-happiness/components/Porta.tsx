e7f445b4980f2565f14b95d7bef4ba15d979db7d
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-29 15:18:00 -0300

    Configuring and using PortalModel in how attribute in Porta

--- door-of-happiness/components/Porta.tsx
+++ door-of-happiness/components/Porta.tsx
@@ -1,14 +1,19 @@
+import PortaModel from "../model/portaModel";
 import styles from "../src/styles/Porta.module.css";
 
-export default function Porta(props) {
+interface PortaProps{
+    porta: PortaModel
+}
 
-    const selecionada = props.selecionada ? styles.selecionada : "";
+export default function Porta(props: PortaProps) {
+    const { porta } = props;
+    const selecionada = porta.selecionada ? styles.selecionada : "";
 
     return (
         <div className={styles.area}>
             <div className={`${styles.estrutura} ${selecionada}`}>
                 <div className={styles.porta}>
-                    <div className={styles.numero}>3</div>
+                    <div className={styles.numero}>{porta.numero}</div>
                     <div className={styles.macaneta}></div>
                 </div>
             </div>
