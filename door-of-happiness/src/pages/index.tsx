6047fd5b001cdd98692da4b2fb5efff750f668e9
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-30 07:33:17 -0300

    Create Jogo Screen and add styles

--- door-of-happiness/src/pages/index.tsx
+++ door-of-happiness/src/pages/index.tsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
-import Porta from "../../components/Porta";
-import PortaModel from "../../model/portaModel";
-import { alterarPorta, criarPortas } from "../../functions/portas";
-
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2));
-
-  const renderizarPortas = () => {
-    return portas.map((porta) => {
-      return (
-        <Porta
-          key={porta.numero}
-          value={porta}
-          onChange={(novaPorta) => {
-            setPortas(alterarPorta(portas, novaPorta));
-          }}
-        />
-      );
-    });
-  };
-  return <div style={{ display: "flex" }}>{renderizarPortas()}</div>;
+  return(
+    <div>
+      <h1>Formulário de Início</h1>
+    </div>
+  );
 }
---
+++ door-of-happiness/src/pages/jogo.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import styles from "../styles/Jogo.module.css";
+import { useState } from "react";
+import { alterarPorta, criarPortas } from "../../functions/portas";
+import Porta from "../../components/Porta";
+import Link from "next/link";
+
+export default function jogo() {
+    const [portas, setPortas] = useState(criarPortas(3, 2));
+
+    const renderizarPortas = () => {
+        return portas.map((porta) => {
+            return (
+                <Porta
+                    key={porta.numero}
+                    value={porta}
+                    onChange={(novaPorta) => {
+                        setPortas(alterarPorta(portas, novaPorta));
+                    }}
+                />
+            );
+        });
+    };
+    return (
+        <div id={styles.jogo}>
+            <div className={styles.portas}>
+            {renderizarPortas()}
+            </div>
+            <div className={styles.botoes}>
+                <Link href="/">
+                    <button>Reiniciar Jogo</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
